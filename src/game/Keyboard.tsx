39890ac11bb34704b76c8a25aa631ba7cf53b8ba
itsvetkoff <iotsvetkov@gmail.com>
2022-11-12 19:43:29 +0200

    layout without main logic

---
+++ src/game/Keyboard.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+
+const rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+
+const buttons = rows.map((row) => row.split(""));
+
+export const Keyboard = ({ onBackspace, onPressed }) => (
+  <div className="keyboard">
+    <div>
+      {buttons.map((row, index) => (
+        <div key={index}>
+          {row.map((letter) => (
+            <button onClick={() => onPressed(letter)} key={letter}>
+              {letter}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+    <div>
+      <button onClick={() => onBackspace()}>Backspace</button>
+    </div>
+  </div>
+);
